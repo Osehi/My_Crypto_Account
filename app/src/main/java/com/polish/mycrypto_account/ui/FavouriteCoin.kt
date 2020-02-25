418eb3df6f581661c0ad9e53a70cfbb5dc28c872
Osehi <osehiaseehilen@gmail.com>
2020-02-25 14:03:36 +0100

    'favouriteViewModel-created'

--- app/src/main/java/com/polish/mycrypto_account/ui/FavouriteCoin.kt
+++ app/src/main/java/com/polish/mycrypto_account/ui/FavouriteCoin.kt
@@ -6,14 +6,21 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 
 import com.polish.mycrypto_account.R
+import com.polish.mycrypto_account.adapter.FavouriteCoinAdapter
 import com.polish.mycrypto_account.databinding.FragmentFavouriteCoinBinding
+import com.polish.mycrypto_account.viewModel.FavouriteCoinViewModel
 
 /**
  * A simple [Fragment] subclass.
  */
 class FavouriteCoin : Fragment() {
+
+    lateinit var favouriteCoinViewModel: FavouriteCoinViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -24,6 +31,25 @@ class FavouriteCoin : Fragment() {
 
         // inflate the layout via binding
         val binding = FragmentFavouriteCoinBinding.inflate(inflater, container, false)
+
+        // create an instance of the viewModel
+        favouriteCoinViewModel = ViewModelProvider(this).get(FavouriteCoinViewModel::class.java)
+
+        // connect the recyclerView to the adapter
+        val recyclerView = binding.recyclerViewFavouriteCoinId
+        recyclerView.layoutManager = LinearLayoutManager(context)
+
+        var adapter = FavouriteCoinAdapter(FavouriteCoinAdapter.OnClickListener{})
+
+        recyclerView.adapter = adapter
+
+        favouriteCoinViewModel.allFavouriteCoin.observe(viewLifecycleOwner, Observer {
+            it?.let {
+                adapter.submitList(it)
+            }
+        })
+
+
 
         return binding.root
 
--- app/src/main/java/com/polish/mycrypto_account/viewModel/FavouriteCoinViewModel.kt
+++ app/src/main/java/com/polish/mycrypto_account/viewModel/FavouriteCoinViewModel.kt
@@ -1,4 +1,25 @@
 package com.polish.mycrypto_account.viewModel
 
-class FavouriteCoinViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import com.polish.mycrypto_account.model.CryptoCoin
+import com.polish.mycrypto_account.repository.FavouriteCoinRepository
+
+class FavouriteCoinViewModel(application: Application): AndroidViewModel(application) {
+
+    private var repository = FavouriteCoinRepository(application)
+
+    val allFavouriteCoin = repository.getAllFavouriteCoin()
+
+
+
+    fun insertFavouriteCoin(cryptoCoin: CryptoCoin){
+        repository.insertFavouriteCoin(cryptoCoin)
+    }
+
+
+    fun deleteFavouriteCoin(cryptoCoin: CryptoCoin){
+        repository.deleteFavouriteCoin(cryptoCoin)
+    }
+
 }
